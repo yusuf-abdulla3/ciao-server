82075b5983a799f24442abcbd239762d8992f238
Yusuf <y.abdulla37@gmail.com>
2023-12-24 08:34:11 -0700

    changes server url. updates packages

--- express_server/app.js
+++ express_server/app.js
@@ -30,7 +30,7 @@ const background = require('./routes/background');
 
 var app = express();
 const corsOptions ={
-  origin:'http://localhost:3000', 
+  origin:'https://ciao-server.onrender.com', 
   credentials:true,            //access-control-allow-credentials:true
   // optionSuccessStatus:200,
 }
--- express_server/db/index.js
+++ express_server/db/index.js
@@ -1,7 +1,7 @@
 const pg = require('pg');
 // require('dotenv').config();
 
-const connectionString = `postgres://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?sslmode=disable` ;
+const connectionString = `${process.env.POSTGRES_URL}?sslmode=require` ;
 
 const client = new pg.Client({
     connectionString: connectionString || process.env.DATABASE_URL,
